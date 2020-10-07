2ac77183eb146d3d886caadc7ddbeb553f1ea60f
Jason <jaylima0@gmail.com>
2020-10-06 21:48:29 -0400

    Add lazy loading routes. Need to verify lazy loading

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,7 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   declarations: [ProfileComponent],

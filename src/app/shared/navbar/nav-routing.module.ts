020d6d95ce8a0081e44651e15271e51f36401c71
Jason <jaylima0@gmail.com>
2020-11-01 21:39:58 -0500

    Add routing to navigation component

---
+++ src/app/shared/navbar/nav-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./../../customer-dashboard/customer-dashboard.module').then(m => m.CustomerDashboardModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./../../profile/profile.module').then(m => m.ProfileModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ]
+})
+export class NavRoutingModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { NavRoutingModule } from "./navbar/nav-routing.module";
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
     CommonModule,
+    NavRoutingModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    NavRoutingModule
   ]
 })
 export class SharedModule { }
